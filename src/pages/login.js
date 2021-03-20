84d40c1ae3e937707f9d28196c55505b887c4376
Ishibashi-Kuramori <kisibasi0331@gmail.com>
2021-03-20 22:24:45 +0900

    画面遷移の仕組みを実装

---
+++ src/pages/login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Login extends Component {
+  render() {
+     return (
+      <div>
+        Login Page!!!<br />
+        <Link to="/">Go To Top</Link>
+      </div>
+    );
+  }
+}
