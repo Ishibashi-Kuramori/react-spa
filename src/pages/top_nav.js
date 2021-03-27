2226c87654e8f87503f404edf5f9050c2a747bd2
Ishibashi-Kuramori <kisibasi0331@gmail.com>
2021-03-27 22:40:27 +0900

    新規登録画面とユーザ削除を実装

--- src/pages/top_nav.js
+++ src/pages/top_nav.js
@@ -7,20 +7,47 @@ export default class TopNav extends Component {
   // コンストラクタ
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isShowModal: false
+    };
   }
 
   // render読み込み後にログイン状態をチェック
   // ※constructorでやるとsetStateで警告が出る
   componentDidMount() {
     firebaseAuth.onAuthStateChanged((user) => {
-      this.setState({ user })
+      this.setState({ user: user })
     })
   }
 
   // Logoutボタンクリック時
   onSignOut = () => {
     firebaseAuth.signOut();
+  }
+
+  // ユーザ削除確認ダイアログ表示
+  showModal = () => {
+    this.setState({
+      isShowModal: true
+    });
+  }
+
+  // ユーザ削除確認ダイアログ非表示
+  hideModal = () => {
+    this.setState({
+      isShowModal: false
+    });
+  }
+
+  // ユーザ削除処理
+  deleteUser = () => {
+    const hideModal = this.hideModal;
+    this.state.user.delete().then(function() {
+      hideModal();
+    }).catch(function(error) {
+      console.log(error);
+      alert('ユーザ削除に失敗しました。\n一旦ログアウトしてもう一度お試し下さい。');
+    });
   }
 
   render() {
@@ -59,12 +86,30 @@ export default class TopNav extends Component {
               {this.state.user &&
               <div className="field is-grouped">
                 <p className="control">
+                    <span className="icon mr-0">
+                      <i className="fa fa-user"></i>
+                    </span>
+                    <strong>
+                      {this.state.user.displayName}
+                    </strong>
+                </p>
+                <p className="control">
                   <button className="button is-small is-info is-outlined" onClick={this.onSignOut}>
                     <span className="icon mr-0">
                       <i className="fas fa-sign-out-alt"></i>
                     </span>
                     <span>
                       Logout
+                    </span>
+                  </button>
+                </p>
+                <p className="control">
+                  <button className="button is-small is-danger" onClick={this.showModal}>
+                    <span className="icon mr-0">
+                      <i className="fas fa-exclamation-triangle"></i>
+                    </span>
+                    <span>
+                      Delete User
                     </span>
                   </button>
                 </p>
@@ -84,6 +129,35 @@ export default class TopNav extends Component {
         </div>
       </div>
 
+      {this.state.user &&
+      <div className={'modal ' + (this.state.isShowModal ? 'is-active' : '')}>
+        <div className="modal-background" onClick={this.hideModal}></div>
+        <div className="modal-content">
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">退会確認</p>
+            <button className="delete" aria-label="close" id="close" onClick={this.hideModal}></button>
+          </header>
+          <section className="modal-card-body">
+            <span className="icon mr-0">
+              <i className="fa fa-user"></i>
+            </span>
+            <strong>
+              {this.state.user.displayName}
+            </strong>
+            <span>
+              さんのユーザ情報、及び投稿データが全て削除されます。<br />
+              よろしいでしょうか？
+            </span>
+          </section>
+          <footer className="modal-card-foot">
+            <button className="button is-danger" onClick={this.deleteUser}>削除</button>
+            <button className="button" onClick={this.hideModal}>キャンセル</button>
+          </footer>
+        </div>
+        </div>
+      </div>
+      }
     </div>);
   }
 }
